c67d7fb241b3c013538fd4359898a8792982c0ea
wrlandev <wrlandev0@gmail.com>
2024-03-25 00:18:18 -0300

    feat: create first test

---
+++ Domain/Entities/TaskEntity.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Domain.Entities
+{
+    public class TaskEntity
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime ExpirationDate { get; private set; }
+        public int Priority { get; private set; }
+        public List<string> Tags { get; private set; }
+
+        public TaskEntity(string title, string description, int priority, List<string> tags, DateTime expirationDate)
+        {
+            Title = title;
+            Description = description;
+            Priority = priority;
+            Tags = tags;
+            ExpirationDate = expirationDate;
+        }
+    }
+}
--- TodoSystem.Tests/Tasks.cs
+++ TodoSystem.Tests/Tasks.cs
@@ -1,11 +1,26 @@
+using Domain.Entities;
+
 namespace TodoSystem.Tests
 {
     public class Tasks
     {
         [Fact]
-        public void Test1()
+        public void CreateTaskSuccessfully()
         {
+            //Arrange
+            var title = "task";
+            var description = "description";
+            var expirationDate = DateTime.Now;
+            var priority = 1;
+            var tags = new List<string> { "Teste1", "Teste2" };
 
+            //Act
+            var task = new TaskEntity(title, description, priority, tags, expirationDate);
+
+            //Assert
+            Assert.Equal(task.Title, title);
+            Assert.Equal(task.Description, description);
+            Assert.Equal(task.ExpirationDate, expirationDate);
         }
     }
 }
